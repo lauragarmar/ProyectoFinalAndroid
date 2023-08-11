b038627e9a3e93390184195b946a5f8319671191
lauragarmar <lauragarmar94@gmail.com>
2023-08-11 09:48:22 +0200

    cambio diseño barra busqueda

--- app/src/main/java/com/example/proyectofinal_v2/presentation/search/SearchCharacterBar.kt
+++ app/src/main/java/com/example/proyectofinal_v2/presentation/search/SearchCharacterBar.kt
@@ -1,5 +1,6 @@
 package com.example.proyectofinal_v2.presentation.search
 
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,6 +16,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.alpha
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
+import com.example.proyectofinal_v2.presentation.theme.PrimaryGreen
 
 @Composable
 fun SearchCharacterBar(
@@ -22,9 +24,9 @@ fun SearchCharacterBar(
     onValueChange: (String) -> Unit,
     onSearchExecute: () -> Unit
 ) {
-    Box(modifier = Modifier.padding(16.dp)) {
-        val searchIcon = @Composable { Icon(Icons.Default.Search, "Icono de búsqueda") }
-        val placeHolderText = @Composable { Text(text = "Buscar...") }
+    Box(modifier = Modifier.padding(16.dp).background(color = Color.Black)) {
+        val searchIcon = @Composable { Icon(Icons.Default.Search, contentDescription = "Icono de búsqueda", tint = PrimaryGreen,) }
+        val placeHolderText = @Composable { Text(text = "Buscar...", color = PrimaryGreen) }
         TextField(
             value = value,
             onValueChange = onValueChange,
@@ -33,7 +35,7 @@ fun SearchCharacterBar(
             keyboardActions = KeyboardActions(),
             placeholder = placeHolderText,
             colors = TextFieldDefaults.textFieldColors(
-                textColor = Color.Black
+                textColor = PrimaryGreen
             )
         )
     }
